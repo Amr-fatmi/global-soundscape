ce85f4b42491dd1952938ce67a5b36fe9a090fdd
Amr Ait El Fatmi <amr.fatmi@gmail.com>
2025-07-19 11:41:15 +0100

    feat: add Chart.js metadata charts and initialize map with default sound pins

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from dotenv import load_dotenv
 import os, requests
+from dotenv import load_dotenv
+from collections import Counter
 
 from flask import Flask, render_template, request, jsonify
 
@@ -14,6 +15,43 @@ FREESOUND_API_KEY = os.getenv("FREESOUND_API_KEY")
 def index():
     return render_template("index.html")
 
+@app.route("/sounds")
+def sounds():
+    url = "https://freesound.org/apiv2/search/text/"
+    params = {
+        "query": "sound",
+        "page": 1,
+        "page_size": 100,
+        "filter": "duration:[0 TO 300] geotag:[* TO *]",
+        "fields": "id,name,geotag,previews,tags,username",
+        "sort": "score_desc",
+        "token": FREESOUND_API_KEY
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+
+        data = response.json()
+
+        print(data["results"])
+
+        results = data["results"]
+        all_tags = []
+
+        for sound in results:
+            all_tags.extend(sound["tags"])
+
+        top_tags = Counter(all_tags).most_common(10)
+        print(top_tags)
+
+        print(f"Data fetched successfully: 200")
+        print(f"Pages: {data["count"]}")
+        return jsonify({"results": results, "top_tags": top_tags}), 200
+    else:
+        return jsonify({"error": "Freesound API failed"}), response.status_code
+
+
 @app.route("/search")
 def search():
     city = request.args.get("city")
@@ -22,14 +60,30 @@ def search():
     params = {
         "query": city,
         "fields": "id,username,name,previews,duration,geotag,created,tags",
+        "page_size": 150,
+        "page": 1,
         "token": FREESOUND_API_KEY
     }
 
     response = requests.get(url, params=params)
 
     if response.status_code == 200:
+
         data = response.json()
+
+        results = data["results"]
+        all_tags = []
+
+        for sound in results:
+            all_tags.extend(sound["tags"])
+
+        top_tags = Counter(all_tags).most_common(10)
+        print(top_tags)
+
+
+        
         print(f"Data fetched successfully: 200")
-        return jsonify({"results": data["results"]}), 200
+        print(f"Pages: {data["count"]}")
+        return jsonify({"results": results, "top_tags": top_tags}), 200
     else:
         return jsonify({"error": "Freesound API failed"}), response.status_code
--- static/script.js
+++ static/script.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
+ let tagsChart = null;
+
     var map = L.map("map").setView([30, 0], 2.5);
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         maxZoom: 19,
@@ -6,36 +8,162 @@ document.addEventListener("DOMContentLoaded", () => {
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(map);
 
+    loadDefaultMap();
+
     document.getElementById("search-form").addEventListener("submit", (event) => {
         event.preventDefault();
 
         let userSearch = document.getElementById("search-input").value;
-        
-        let params = new URLSearchParams();
-        params.append("city", userSearch);
 
-        fetch(`/search?${params.toString()}`)
-            .then(res => res.json())
-            .then(data => {
-                for (let i = 0; i < data.results.length; i++) {
-                    if (data.results[i].geotag != null) {
-                        let geotag = data.results[i].geotag.split(" ");
+        let geoParams = new URLSearchParams({
+        city: userSearch,
+        format: "json",
+        limit: 1,
+        });
 
-                        let lat = Number(geotag[0]);
-                        let lon = Number(geotag[1]);
+        fetch(
+        `https://nominatim.openstreetmap.org/search?q=${geoParams.toString()}`
+        )
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.length > 0) {
+                map.setView([data[0].lat, data[0].lon], 10);
+            } else {
+                alert("Location Not Found");
+            }
+        });
 
-                        let popupWindow = `
-                            <b>${data.results[i].name}</b><br>
-                            <b>By: ${data.results[i].username}</b><br>
-                            <b>Tags: ${data.results[i].tags[0]}, ${data.results[i].tags[1]}</b><br>
-                            <audio controls src="${data.results[i].previews["preview-hq-mp3"]}"></audio>
+        let appParams = new URLSearchParams({
+            city: userSearch,
+        });
+
+        fetch(`/search?${appParams.toString()}`)
+        .then((res) => res.json())
+        .then((data) => {
+            if (tagsChart) {
+                tagsChart.destroy();
+            }
+            tagsChart = createTagsChart(data.top_tags);
+            
+            createFiltersField(data.top_tags);
+
+            for (let i = 0; i < data.results.length; i++) {
+                if (data.results[i].geotag != null) {
+                    let geotag = data.results[i].geotag.split(" ");
+
+                    let lat = Number(geotag[0]);
+                    let lon = Number(geotag[1]);
+
+                    let popupWindow = `
+                        <b>${data.results[i].name}</b><br>
+                        <b>By: ${data.results[i].username}</b><br>
+                        <b>Tags: ${data.results[i].tags[0]}, ${data.results[i].tags[1]}</b><br>
+                        <audio controls src="${data.results[i].previews["preview-hq-mp3"]}"></audio>
                         `;
 
-                        let marker = L.marker([lat, lon]).addTo(map);
-                        marker.bindPopup(popupWindow).openPopup();
-                    } 
+                    let marker = L.marker([lat, lon]).addTo(map);
+                    marker.bindPopup(popupWindow).openPopup();
                 }
-                console.log(data.results)
-            })
-    })
+            }
+        });
+    });
+
+    function createTagsChart(top_tags) {
+        const ctx = document.getElementById("myChart");
+
+        let tags = []
+        let data = []
+        for (let i = 0; i < top_tags.length; i++) {
+            tags.push(top_tags[i][0])
+            data.push(top_tags[i][1])
+        }
+
+        console.log(tags);
+
+        let chart = new Chart(ctx, {
+            type: "bar",
+            data: {
+                labels: tags,
+                datasets: [
+                {
+                    label: "Top Tags",
+                    data: data,
+                    borderWidth: 1,
+                },
+                ],
+            },
+            options: {
+                indexAxis: 'y',
+                scales: {
+                y: {
+                    beginAtZero: true,
+                },
+                },
+            },
+        });
+
+        return chart
+    }
+
+    function createFiltersField(top_tags) {
+
+        let field = document.getElementById("tags-field");
+        field.innerHTML = "";
+
+        for (let i = 0; i < top_tags.length; i++) {
+            let checkbox = document.createElement("input");
+            checkbox.setAttribute("type", "checkbox");
+            checkbox.setAttribute("name", "tags");
+            checkbox.setAttribute("id", top_tags[i][0]);
+            checkbox.setAttribute("value", top_tags[i][0]);
+
+            let label = document.createElement("label");
+            label.setAttribute("for", top_tags[i][0]);
+            label.innerHTML = top_tags[i][0];
+
+            let containerDiv = document.createElement("div");
+            containerDiv.className = "checkbox-group";
+            containerDiv.appendChild(checkbox);
+            containerDiv.appendChild(label);
+
+            field = document.getElementById("tags-field");
+            field.appendChild(containerDiv);
+        }
+    }
+
+    function loadDefaultMap() {
+        fetch(`/sounds`)
+        .then((res) => res.json())
+        .then((data) => {
+            if (tagsChart) {
+                tagsChart.destroy();
+            }
+            tagsChart = createTagsChart(data.top_tags);
+            
+            createFiltersField(data.top_tags);
+
+            console.log(data.results);
+
+            for (let i = 0; i < data.results.length; i++) {
+                if (data.results[i].geotag != null) {
+                    let geotag = data.results[i].geotag.split(" ");
+
+                    let lat = Number(geotag[0]);
+                    let lon = Number(geotag[1]);
+
+                    let popupWindow = `
+                        <b>${data.results[i].name}</b><br>
+                        <b>By: ${data.results[i].username}</b><br>
+                        <b>Tags: ${data.results[i].tags[0]}, ${data.results[i].tags[1]}</b><br>
+                        <audio controls src="${data.results[i].previews["preview-hq-mp3"]}"></audio>
+                        `;
+
+                    let marker = L.marker([lat, lon]).addTo(map);
+                    marker.bindPopup(popupWindow).openPopup();
+                }
+            }
+        });
+    };
+
 });
+
